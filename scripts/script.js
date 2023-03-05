8d0e6063ad35bd24bac9d7c26268d3d1d2c71c77
eduardo2382 <motadossantosjoseeduardo.m82@gmail.com>
2023-03-05 20:16:16 -0300

    teste da animacao

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,21 @@
-const botao = document.getElementById('btnComecar')
-const calculoEstado = document.getElementById('calculoEstado')
-const calculoinicio = document.getElementById('calculoinicio')
+const btnCalculo = document.querySelectorAll('.btnCalculo')
+const areaCalculo = document.getElementsByClassName('areaCalculo')
 
-botao.addEventListener('click', function(){
-    calculoinicio.classList.add('animateSair')
-    calculoEstado.classList.add('animateEntrar')
+
+btnCalculo.forEach(function(btn, i){
+    btn.addEventListener('click', function(){
+        animateSair(areaCalculo[i], i+1)
+        animateEntrar(areaCalculo[i+1], i+1)
+        console.log('ola')
+    })
 })
+
+function animateSair(elemento, vezes=1){
+    elemento.style.transform = `translateX(${-100 * vezes}%)`
+    elemento.style.opacity = 0
+}
+
+function animateEntrar(elemento, vezes=1){
+    elemento.style.transform = `translateX(${-100 * vezes}%)`
+    elemento.style.opacity = 1
+}
