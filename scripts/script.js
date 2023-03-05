3166914d16a308845f1d14c2e1dfc71b0ab835b8
eduardo2382 <motadossantosjoseeduardo.m82@gmail.com>
2023-03-04 21:41:09 -0300

    teste de animacao no mobile

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+const btn_comecar = document.getElementById('btnComecar')
+const calculoinicio = document.getElementById('calculoinicio') 
+var calculoestado = document.getElementById('calculoestado') 
+
+const delay = 500
+
+var etapa = ['estado', 'aparelho', 'potencia', 'finalizar']
+
+btn_comecar.addEventListener('click', function(){
+    calculoinicio.classList.add('animateSair')
+    setTimeout(function(){
+        calculoinicio.style.display = 'none'
+        calculoestado.classList.add('animateEntrar')
+        calculoestado.style.display = 'flex'
+    }, delay)
+})
+
