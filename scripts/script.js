7b7e27606bd226e63a4f4b8311aa4123c1132445
eduardo2382 <motadossantosjoseeduardo.m82@gmail.com>
2023-03-06 22:13:05 -0300

    teste de animação de entrada e saida no mobile

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,47 @@
 const btnAvancar = document.querySelectorAll('.btnAvancar')
+const btnVoltar = document.querySelectorAll('.btnVoltar')
 const areaCalculo = document.getElementsByClassName('areaCalculo')
+const selectAparelho = document.getElementById('selectAparelho')
 
+btnVoltar.forEach(function(btn, i){
+    btn.addEventListener('click', function(){
+        if(i == 0){
+            animateEntrar(areaCalculo[i], 0, 'direita')
+            animateSair(areaCalculo[i+1], 0, 'direita')
+        } else{
+            animateEntrar(areaCalculo[i], 1, 'direita')
+            animateSair(areaCalculo[i+1], 0, 'direita')
+        }
+    })
+})
 
 btnAvancar.forEach(function(btn, i){
     btn.addEventListener('click', function(){
         if(i == btnAvancar.length - 1){
             console.log("acabou")
         } else{
-            console.log(i)
-            animateSair(areaCalculo[i], i+1)
-            animateEntrar(areaCalculo[i+1], i+1)
+            animateSair(areaCalculo[i], i+1, 'esquerda')
+            animateEntrar(areaCalculo[i+1], i+1, 'esquerda')
         }
     })
 })
 
-function animateSair(elemento, vezes=1){
-    elemento.style.transform = `translateX(${-100 * vezes}%)`
-    elemento.style.opacity = 0
+function animateSair(elemento, vezes=1, lado){
+    if(lado == 'esquerda'){
+        elemento.style.transform = `translateX(${-100 * vezes}%)`
+        elemento.style.opacity = 0
+    } else{
+        elemento.style.transform = `translateX(${+100 * vezes}%)`
+        elemento.style.opacity = 0
+    }
 }
 
-function animateEntrar(elemento, vezes=1){
-    elemento.style.transform = `translateX(${-100 * vezes}%)`
-    elemento.style.opacity = 1
+function animateEntrar(elemento, vezes=1, lado){
+    if(lado == 'esquerda'){
+        elemento.style.transform = `translateX(${-100 * vezes}%)`
+        elemento.style.opacity = 1
+    } else{
+        elemento.style.transform = `translateX(${-100 * vezes}%)`
+        elemento.style.opacity = 1
+    }
 }
