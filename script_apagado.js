6a561bddd89fae3211f10972cc0e042714555a6d
eduardo2382 <motadossantosjoseeduardo.m82@gmail.com>
2023-05-15 19:13:47 -0300

    criando a tabela do resultado

---
+++ script_apagado.js
@@ -0,0 +1,134 @@
+const btnAvancar = document.querySelectorAll('.btnAvancar')
+const btnVoltar = document.querySelectorAll('.btnVoltar')
+
+
+const sectionCalculo = document.getElementById('sectionCalculo')
+const calculoNovoAparelho = document.getElementById('calculoNovoAparelho')
+
+const nomeNovoAparelho = document.getElementById('nomeNovoAparelho')
+const potenciaNovoAparelho = document.getElementById('potenciaNovoAparelho')
+
+
+const selectEstado = document.getElementById('selectEstado')
+const selectAparelho = document.getElementById('selectAparelho')
+
+
+avancar()
+
+
+
+btnVoltar.forEach(function(btn, i){
+    btn.addEventListener('click', function(){
+        let areaCalculo = document.getElementsByClassName('areaCalculo')
+
+        if(i == 0){
+            animateEntrar(areaCalculo[i], 0, 'direita')
+            animateSair(areaCalculo[i+1], 0, 'direita')
+        } else{
+            animateEntrar(areaCalculo[i], 1, 'direita')
+            animateSair(areaCalculo[i+1], 0, 'direita')
+        }
+    })
+})
+
+selectAparelho.addEventListener('change', function(){
+    
+    
+    avancar()
+})
+
+function avancar(){
+    btnAvancar.forEach(function(btn, i){
+        btn.addEventListener('click', function(){
+
+            if(selectAparelho.value == 'outro'){
+                if(i == 1){
+                    if(verificarSelect(selectEstado)){
+                        animatePassarArea(i)
+                    }
+                } else if(i == 2){
+                    if(verificarSelect(selectAparelho)){
+                        animatePassarArea(i)
+                        
+                    }
+                } else if(i == 3){
+                    if(verificarInput(nomeNovoAparelho) && verificarInput(potenciaNovoAparelho)){
+                        animatePassarArea(i)
+                    }
+                } else{
+                    animatePassarArea(i)
+                }
+            } else{
+                if(i == 1){
+                    if(verificarSelect(selectEstado)){
+                        animatePassarArea(i)
+                    }
+                } else if(i == 2){
+                    if(verificarSelect(selectAparelho)){
+                        animatePassarArea(i)
+                    }
+                } else if(i == 3){
+                    if(verificarInput(nomeNovoAparelho) && verificarInput(potenciaNovoAparelho)){
+                        animatePassarArea(i)
+                    }
+                } else{
+                    animatePassarArea(i)
+                }
+            }
+
+    })
+})
+}
+
+function animateSair(elemento, vezes=1, lado){
+    if(lado == 'esquerda'){
+        elemento.style.transform = `translateX(${-100 * vezes}%)`
+        elemento.style.opacity = 0
+    } else{
+        elemento.style.transform = `translateX(${+100 * vezes}%)`
+        elemento.style.opacity = 0
+    }
+}
+
+function animateEntrar(elemento, vezes=1, lado){
+    if(lado == 'esquerda'){
+        elemento.style.transform = `translateX(${-100 * vezes}%)`
+        elemento.style.opacity = 1
+    } else{
+        elemento.style.transform = `translateX(${-100 * vezes}%)`
+        elemento.style.opacity = 1
+    }
+}
+
+function verificarSelect(select){
+    let optionSelected = select.options[select.selectedIndex].value
+
+    if(optionSelected == 'invalid'){
+        return false
+    } else{
+        return optionSelected
+    }
+}
+
+function verificarInput(input){
+    if(input.value.length > 0){
+        return true
+    } else{
+        return false
+    }
+}
+
+function animatePassarArea(i){
+    let areaCalculo = document.getElementsByClassName('areaCalculo')
+
+    if(i == btnAvancar.length - 1){
+        console.log("acabou")
+    } else{
+        animateSair(areaCalculo[i], i+1, 'esquerda')
+        animateEntrar(areaCalculo[i+1], i+1, 'esquerda')
+    }
+}
+
+
+
+
--- scripts/script.js
+++ scripts/script.js
@@ -1,110 +1,87 @@
-const btnAvancar = document.querySelectorAll('.btnAvancar')
-const btnVoltar = document.querySelectorAll('.btnVoltar')
-
-
-const sectionCalculo = document.getElementById('sectionCalculo')
-const calculoNovoAparelho = document.getElementById('calculoNovoAparelho')
-
-const nomeNovoAparelho = document.getElementById('nomeNovoAparelho')
-const potenciaNovoAparelho = document.getElementById('potenciaNovoAparelho')
-
-
-const selectEstado = document.getElementById('selectEstado')
-const selectAparelho = document.getElementById('selectAparelho')
-
-
-avancar()
+var areasCalculo = document.querySelectorAll('.areaCalculo');
+var btnAvancar = document.querySelectorAll('.btnAvancar')
+var btnVoltar = document.querySelectorAll('.btnVoltar')
+var novoAparelho = document.getElementById('calculoNovoAparelho')
+var selectAparelho = document.getElementById('selectAparelho')
+var selectEstado = document.getElementById('selectEstado')
 
 
 
-btnVoltar.forEach(function(btn, i){
-    btn.addEventListener('click', function(){
-        let areaCalculo = document.getElementsByClassName('areaCalculo')
+btnAvancar.forEach( (item, index) => { // obtem todos os botões avancar
+    item.addEventListener('click', () => { // quando um botão avancar for clicado ele chama a função "avancar"
+        
+        switch(index) {
+            case 0:
+                avancar(index)
+                break;
 
-        if(i == 0){
-            animateEntrar(areaCalculo[i], 0, 'direita')
-            animateSair(areaCalculo[i+1], 0, 'direita')
-        } else{
-            animateEntrar(areaCalculo[i], 1, 'direita')
-            animateSair(areaCalculo[i+1], 0, 'direita')
+            case 1:
+                verificarSelect(selectEstado) ? avancar(index) : undefined
+                break;
+
+            case 2:
+                if(verificarSelect(selectAparelho)){
+                    if(verificarSelect(selectAparelho) == 'outro'){
+                        if(verificarInput(document.getElementById('potenciaNovoAparelho'))){
+                            avancar(index)
+                        }
+                    } else{
+                        avancar(index)
+                    }
+                }
+
+                break
+
+            case 3:
+                avancar(index)
+        
+            default:
+                break;
         }
+    })
+})
+
+
+btnVoltar.forEach( (item, index) => { // obtem todos os botões voltar
+    item.addEventListener('click', () => { // quando um botão voltar for clicado ele chama a função "voltar"
+        voltar(index)
     })
 })
 
 selectAparelho.addEventListener('change', function(){
     let optionSelected = selectAparelho.options[selectAparelho.selectedIndex].value
-    let avisoAparelho = document.getElementById('avisoAparelho')
-
 
     if(optionSelected == 'outro'){
-        calculoNovoAparelho.style.display = 'inline-block'
-        calculoNovoAparelho.classList.add('areaCalculo')
-        avisoAparelho.style.display = 'block'
+        document.getElementById('areaNovoAparelho').style.display = 'block'
     } else{
-            calculoNovoAparelho.style.display = 'none'
-            calculoNovoAparelho.classList.remove('areaCalculo')
-            avisoAparelho.style.display = 'none'
+        document.getElementById('areaNovoAparelho').style.display = 'none'
     }
-    
-    avancar()
 })
 
-function avancar(){
-    btnAvancar.forEach(function(btn, i){
-        btn.addEventListener('click', function(){
-
-            if(selectAparelho.value == 'outro'){
-                if(i == 1){
-                    if(verificarSelect(selectEstado)){
-                        animatePassarArea(i)
-                    }
-                } else if(i == 2){
-                    if(verificarSelect(selectAparelho)){
-                        animatePassarArea(i)
-                        
-                    }
-                } else if(i == 3){
-                    if(verificarInput(nomeNovoAparelho) && verificarInput(potenciaNovoAparelho)){
-                        animatePassarArea(i)
-                    }
-                } else{
-                    animatePassarArea(i)
-                }
-            } else{
-                if(i == 1){
-                    if(verificarSelect(selectEstado)){
-                        animatePassarArea(i)
-                    }
-                } else if(i == 2){
-                    if(verificarSelect(selectAparelho)){
-                        animatePassarArea(i)
-                    }
-                } else{
-                    animatePassarArea(i)
-                }
-            }
-
-    })
-})
-}
-
-function animateSair(elemento, vezes=1, lado){
-    if(lado == 'esquerda'){
-        elemento.style.transform = `translateX(${-100 * vezes}%)`
-        elemento.style.opacity = 0
+function avancar(index){ // funcao para avancar os sections
+    if(index == 0){
+        areasCalculo.forEach( (item) => {
+            item.style.transform = `translateX(-100%)`
+        })
     } else{
-        elemento.style.transform = `translateX(${+100 * vezes}%)`
-        elemento.style.opacity = 0
+        areasCalculo.forEach( (item) => {
+            item.style.transform = `translateX(${-100 * (index + 1)}%)`
+            
+        })
     }
 }
 
-function animateEntrar(elemento, vezes=1, lado){
-    if(lado == 'esquerda'){
-        elemento.style.transform = `translateX(${-100 * vezes}%)`
-        elemento.style.opacity = 1
+function voltar(index){ // funcao para voltar os sections
+    areasCalculo = document.querySelectorAll('.areaCalculo'); 
+
+    if(index == 0){
+        areasCalculo.forEach((item) => {
+            item.style.transform = `translateX(${+100 * 0}%)`
+        })
     } else{
-        elemento.style.transform = `translateX(${-100 * vezes}%)`
-        elemento.style.opacity = 1
+        areasCalculo.forEach((item) => {
+            item.style.transform = `translateX(${-100 * index}%)`
+        })
     }
 }
 
@@ -114,29 +91,14 @@ function verificarSelect(select){
     if(optionSelected == 'invalid'){
         return false
     } else{
-        return true
+        return optionSelected
     }
 }
 
 function verificarInput(input){
-    if(input.value.length > 0){
+    let inputValue = input.value
+
+    if(inputValue.length != 0){
         return true
-    } else{
-        return false
     }
 }
-
-function animatePassarArea(i){
-    let areaCalculo = document.getElementsByClassName('areaCalculo')
-
-    if(i == btnAvancar.length - 1){
-        console.log("acabou")
-    } else{
-        animateSair(areaCalculo[i], i+1, 'esquerda')
-        animateEntrar(areaCalculo[i+1], i+1, 'esquerda')
-    }
-}
-
-
-
-
